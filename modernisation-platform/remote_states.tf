c4f4fac22f386f13c7f3d4cc6381e3c249fb9b86
Piotr Grzeskowiak <piotr.grzeskowiak@digital.justice.gov.uk>
2021-10-25 11:51:41 +0100

    WIP update subnet to allow deploys to shared services

--- modernisation-platform/remote_states.tf
+++ modernisation-platform/remote_states.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "modernisation-platform-repo" {
   backend = "s3"
   config  = {
     bucket = "modernisation-platform-terraform-state"
-    key = "environments/accounts/core-sandbox/core-sandbox-dev/terraform.tfstate"
+    key = "environments/accounts/core-shared-services/core-shared-services-production/terraform.tfstate"
     region = "eu-west-2"
   }
 }
