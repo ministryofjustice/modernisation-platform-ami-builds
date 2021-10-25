bf85bae2e40704558bb53b77014925cda92808a0
Piotr Grzeskowiak <piotr.grzeskowiak@digital.justice.gov.uk>
2021-10-25 11:50:30 +0100

    update method of getting ebs encryption cmk

---
+++ teams/team1/data.tf
@@ -0,0 +1,4 @@
+# Retrieve KMS key for AMI/snapshot encryption
+data "aws_kms_key" "ebs_encryption_cmk" {
+  key_id = "arn:aws:kms:eu-west-2:${data.aws_caller_identity.current.account_id}:alias/ebs-encryption-key"
+}
