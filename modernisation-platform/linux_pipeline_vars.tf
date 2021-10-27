9df9af4b93492db91f4a6b43261b0a33c37137bf
Seb Norris <sebastian@automationlogic.com>
2021-10-27 09:18:42 +0100

    medium stig

--- modernisation-platform/linux_pipeline_vars.tf
+++ modernisation-platform/linux_pipeline_vars.tf
@@ -10,7 +10,7 @@ locals {
     recipe = {
       name         = "MP_AmazonLinux2"
       parent_image = "arn:aws:imagebuilder:eu-west-2:aws:image/amazon-linux-2-x86/x.x.x"
-      version      = "1.0.2"
+      version      = "1.0.5"
       device_name  = "/dev/xvda"
 
       ebs = {
@@ -46,7 +46,7 @@ locals {
       "amazon-cloudwatch-agent-linux",
       "update-linux-kernel-mainline",
       "update-linux",
-      "stig-build-linux-high",
+      "stig-build-linux-medium",
       "inspector-test-linux",
       "reboot-test-linux",
       "reboot-linux"
