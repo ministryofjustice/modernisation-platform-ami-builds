c5779d4a42a6f7446c94e207a475081c3ca780ee
Seb Norris <sebastian@automationlogic.com>
2021-10-29 14:55:26 +0100

    remove random selection of subnet

--- modernisation-platform/base_variables.tf
+++ modernisation-platform/base_variables.tf
@@ -3,9 +3,3 @@ variable "networking" {
   type = list(any)
 
 }
-
-resource "random_shuffle" "image_builder_subnet_ids" {
-  input        = data.terraform_remote_state.modernisation-platform-repo.outputs.image_builder_subnet_ids
-  result_count = 1
-
-}
