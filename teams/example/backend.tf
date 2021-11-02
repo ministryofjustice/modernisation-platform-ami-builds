ae8c15823c895e59469be33267b7670152736062
Seb Norris <sebastian@automationlogic.com>
2021-11-02 14:33:29 +0000

    variablise team name

--- teams/example/backend.tf
+++ teams/example/backend.tf
@@ -6,8 +6,8 @@ terraform {
     acl                  = "bucket-owner-full-control"
     bucket               = "modernisation-platform-terraform-state"
     encrypt              = true
-    key                  = "imagebuilder-team1.tfstate"
+    key                  = join("", ["imagebuilder-", local.team_name, ".tfstate"])
     region               = "eu-west-2"
-    workspace_key_prefix = "environments/accounts/core-shared-services" # This will store the object as environments/core-shared-services/${workspace}/imagebuilder-team1.tfstate
+    workspace_key_prefix = "environments/accounts/core-shared-services" # This will store the object as environments/core-shared-services/${workspace}/imagebuilder-[team name].tfstate
   }
 }
