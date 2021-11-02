dd363102cbbb926a4bfb8094aad4c88bc1a4cf15
Seb Norris <sebastian@automationlogic.com>
2021-11-02 17:00:55 +0000

    team example use subnet output

--- teams/example/windows_pipeline_vars.tf
+++ teams/example/windows_pipeline_vars.tf
@@ -9,7 +9,7 @@ locals {
 
     recipe = {
       name         = join("", [local.team_name, "_WindowsServer2022"])
-      parent_image = "arn:aws:imagebuilder:eu-west-2:763252494486:image/mp-windowsserver2022/x.x.x"
+      parent_image = "arn:aws:imagebuilder:eu-west-2:${data.aws_caller_identity.current.account_id}:image/mp-windowsserver2022/x.x.x"
       version      = "1.0.1"
       device_name  = "/dev/sda1"
 
@@ -25,8 +25,8 @@ locals {
       description        = "Description here"
       instance_types     = ["t2.nano", "t3.micro"]
       name               = join("", [local.team_name, "_WindowsServer2022"])
-      security_group_ids = ["sg-0c2fc68feb53f0122"]
-      subnet_id          = "subnet-07e6dac6dd1c1e8b5"
+      security_group_ids = [data.terraform_remote_state.modernisation-platform-repo.outputs.image_builder_security_group_id]
+      subnet_id          = "${data.terraform_remote_state.modernisation-platform-repo.outputs.non_live_private_subnet_ids[0]}"
       terminate_on_fail  = true
     }
 
