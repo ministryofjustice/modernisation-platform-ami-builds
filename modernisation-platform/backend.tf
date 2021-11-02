d7ffa8406c6e5bc66769bff0b423ffd6dafda4c9
Seb Norris <sebastian@automationlogic.com>
2021-11-02 17:00:55 +0000

    team example use subnet output

--- modernisation-platform/backend.tf
+++ modernisation-platform/backend.tf
@@ -6,8 +6,8 @@ terraform {
     acl                  = "bucket-owner-full-control"
     bucket               = "modernisation-platform-terraform-state"
     encrypt              = true
-    key                  = "imagebuilder.tfstate"
+    key                  = "imagebuilder-mp.tfstate"
     region               = "eu-west-2"
-    workspace_key_prefix = "environments/accounts/core-sandbox" # This will store the object as environments/core-shared-services/${workspace}/imagebuilder-mp.tfstate
+    workspace_key_prefix = "environments/accounts/core-shared-services" # This will store the object as environments/core-shared-services/${workspace}/imagebuilder-mp.tfstate
   }
 }
