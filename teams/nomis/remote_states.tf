86fb01824735dc02e60ca45122f2ac894b820b08
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-02-04 08:50:13 +0000

    DSO-1348 first attempt at RHEL6 AMi pipeline.

---
+++ teams/nomis/remote_states.tf
@@ -0,0 +1,17 @@
+data "terraform_remote_state" "modernisation-platform-repo" {
+  backend = "s3"
+  config = {
+    bucket = "modernisation-platform-terraform-state"
+    key    = "environments/accounts/core-shared-services/core-shared-services-production/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+data "terraform_remote_state" "mp-imagebuilder" {
+  backend = "s3"
+  config = {
+    bucket = "modernisation-platform-terraform-state"
+    key    = "environments/accounts/core-shared-services/core-shared-services-production/imagebuilder-mp.tfstate"
+    region = "eu-west-2"
+  }
+}
---
+++ teams/nomis/rhel6_pipeline_vars.tf
@@ -0,0 +1,50 @@
+locals {
+
+  rhel6_pipeline = {
+
+    pipeline = {
+      name     = join("", [local.team_name, "_RHEL6_10"])
+      schedule = "cron(0 * * * *)"
+    }
+
+    recipe = {
+      name         = join("", [local.team_name, "_RHEL6_10"])
+      parent_account = "309956199498" #RedHat
+      # parent_image = "arn:aws:imagebuilder:eu-west-2:${data.aws_caller_identity.current.account_id}:image/mp-amazonlinux2/x.x.x"
+      # version      = "1.0.1"
+      device_name  = "/dev/sda1"
+
+      ebs = {
+        delete_on_termination = true
+        volume_size           = 30
+        volume_type           = "gp2"
+        encrypted             = false
+      }
+    }
+
+    infra_config = {
+      description        = "RHEL 6_10 Base Image for Weblogic"
+      instance_types     = ["t2.medium"]
+      name               = join("", [local.team_name, "_RHEL6_10"])
+      security_group_ids = [data.terraform_remote_state.modernisation-platform-repo.outputs.image_builder_security_group_id]
+      subnet_id          = "${data.terraform_remote_state.modernisation-platform-repo.outputs.non_live_private_subnet_ids[0]}"
+      terminate_on_fail  = true
+    }
+
+    distribution = {
+      name     = join("", [local.team_name, "_RHEL6_10"])
+      region   = "eu-west-2"
+      ami_name = join("", [local.team_name, "_RHEL6_10_{{ imagebuilder:buildDate }}"])
+    }
+
+    components = [
+      "rhel6.yml"
+    ]
+
+    aws_components = [
+      # "yum-repository-test-linux"
+    ]
+
+  }
+
+}
