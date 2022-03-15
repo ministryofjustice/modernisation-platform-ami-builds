b3d59ac5330e81c9229dd32a6f2b08340ccbb7a8
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-03-15 10:32:06 +0000

    Trying alternative urls.

--- teams/nomis/rhel6_pipeline.tf
+++ teams/nomis/rhel6_pipeline.tf
@@ -2,7 +2,8 @@ locals {
   user_data = <<EOF
 #!/bin/bash
 cd /tmp
-curl https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/3.0.1390.0/linux_amd64/amazon-ssm-agent.rpm --output ssm-agent.rpm
+curl -v https://s3.eu-west-2.amazonaws.com/amazon-ssm-eu-west-2/3.0.1390.0/linux_amd64/
+curl https://s3.eu-west-2.amazonaws.com/amazon-ssm-eu-west-2/3.0.1390.0/linux_amd64/amazon-ssm-agent.rpm --output ssm-agent.rpm
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/3.0.1390.0/linux_amd64/amazon-ssm-agent.rpm
 sudo yum install -y ./ssm-agent.rpm
 sudo start amazon-ssm-agent
