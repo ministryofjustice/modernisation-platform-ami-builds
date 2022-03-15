925b548bfa717ffe687e8be78983c229ef21fdd4
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-03-15 14:16:39 +0000

    Cloudwatch agent in userdata.

--- teams/nomis/rhel6_pipeline.tf
+++ teams/nomis/rhel6_pipeline.tf
@@ -5,6 +5,8 @@ cd /tmp
 curl -v https://s3.eu-west-2.amazonaws.com/amazon-ssm-eu-west-2/3.0.1390.0/linux_amd64/
 sudo yum install -y https://s3.eu-west-2.amazonaws.com/amazon-ssm-eu-west-2/3.0.1390.0/linux_amd64/amazon-ssm-agent.rpm
 sudo start amazon-ssm-agent
+wget https://s3.region.amazonaws.com/amazoncloudwatch-agent-region/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
 EOF
 }
 
