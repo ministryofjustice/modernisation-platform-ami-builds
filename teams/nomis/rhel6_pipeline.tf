34fa8b3ea46d0d0e4bfb3ab66cef99527d34ccee
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-03-16 10:01:48 +0000

    Fixing weblogic component.

--- teams/nomis/rhel6_pipeline.tf
+++ teams/nomis/rhel6_pipeline.tf
@@ -2,7 +2,6 @@ locals {
   user_data = <<EOF
 #!/bin/bash
 cd /tmp
-curl -v https://s3.eu-west-2.amazonaws.com/amazon-ssm-eu-west-2/3.0.1390.0/linux_amd64/
 sudo yum install -y https://s3.eu-west-2.amazonaws.com/amazon-ssm-eu-west-2/3.0.1390.0/linux_amd64/amazon-ssm-agent.rpm
 sudo start amazon-ssm-agent
 wget https://s3.eu-west-2.amazonaws.com/amazoncloudwatch-agent-eu-west-2/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
