4ca05d9ca55955dd54dfa70808b76e022df2fb73
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2022-06-15 15:40:32 +0100

    fix version local

--- teams/nomis/jumpserver_pipeline_vars.tf
+++ teams/nomis/jumpserver_pipeline_vars.tf
@@ -1,15 +1,15 @@
 locals {
+  version_js = "1.0.0"
   jumpserver_pipeline = {
-    version = "1.0.0"
     pipeline = {
-      name     = join("", [local.team_name, "_jumpserver_", replace(local.jumpserver_pipeline.version, ".","_")])
+      name     = join("", [local.team_name, "_jumpserver_", replace(local.version_js, ".","_")])
       schedule = "cron(0 0 2 * ? *)"
     }
 
     recipe = {
       name         = join("", [local.team_name, "_WindowsServer2022"])
       parent_image = "arn:aws:imagebuilder:eu-west-2:${data.aws_caller_identity.current.account_id}:image/mp-windowsserver2022/x.x.x"
-      version      = local.jumpserver_pipeline.version
+      version      = local.version_js
       device_name  = "/dev/sda1"
 
       ebs_block_device = [
@@ -27,14 +27,14 @@ locals {
     infra_config = {
       description        = "Windows Server 2022 Image for Jumpserver"
       instance_types     = ["t3.large"]
-      name               = join("", [local.team_name, "_jumpserver_",replace(local.jumpserver_pipeline.version, ".","_")])
+      name               = join("", [local.team_name, "_jumpserver_",replace(local.version_js, ".","_")])
       security_group_ids = [data.terraform_remote_state.modernisation-platform-repo.outputs.image_builder_security_group_id]
       subnet_id          = "${data.terraform_remote_state.modernisation-platform-repo.outputs.non_live_private_subnet_ids[0]}"
       terminate_on_fail  = true
     }
 
     distribution = {
-      name     = join("", [local.team_name, "_jumpserver_", replace(local.jumpserver_pipeline.version, ".","_")])
+      name     = join("", [local.team_name, "_jumpserver_", replace(local.version_js, ".","_")])
       region   = "eu-west-2"
       ami_name = join("", [local.team_name, "_jumpserver_{{ imagebuilder:buildDate }}"])
     }
