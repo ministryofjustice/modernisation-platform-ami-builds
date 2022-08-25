e470ddc19a9fd3b17de849e4b1d7ab7639105596
wullub <william@automationlogic.com>
2022-08-25 15:55:20 +0100

    make rhel 6.10 base ami
    make rhel 6.10 base ami

--- teams/nomis/imagebuilders_vars.tf
+++ teams/nomis/imagebuilders_vars.tf
@@ -1,5 +1,6 @@
 locals {
   imagebuilders = {
     "RHEL7-9_BaseImage" = local.RHEL7-9_BaseImage
+    "rhel_6_10_base"    = local.rhel_6_10_base
   }
 }
---
+++ teams/nomis/rhel_6_10_base_vars.tf
@@ -0,0 +1,49 @@
+locals {
+  rhel_6_10_base = {
+    configuration_version = "0.0.1"
+    description           = "nomis rhel 6.10 base image"
+
+    tags = {
+      os-version = "rhel 6.10"
+    }
+
+    image_recipe = {
+      parent_image = {
+        owner             = "309956199498" # Redhat
+        filter_name_value = "RHEL-6.10_HVM-*"
+      }
+      block_device_mappings_ebs = [
+        {
+          device_name = "/dev/sda1" # root volume
+          volume_size = 30
+          volume_type = "gp3"
+        }
+      ]
+      components_aws = [
+        "update-linux",
+        "stig-build-linux-medium",
+        "aws-cli-version-2-linux",
+        "amazon-cloudwatch-agent-linux"
+      ]
+      components_custom = [
+        "components/rhel_6_10_base.yml"
+      ]
+    }
+
+    infrastructure_configuration = {
+      instance_types = ["t2.large"]
+    }
+
+    distribution_configuration = {
+      ami_distribution_configuration = {
+        target_account_ids = local.ami_share_accounts
+      }
+    }
+
+    image_pipeline = {
+      schedule = {
+        schedule_expression = "cron(0 0 1 * ? *)"
+      }
+    }
+  }
+}
