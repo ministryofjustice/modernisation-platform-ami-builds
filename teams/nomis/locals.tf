8d8cffc6f83c6a4c37c6889fc8d10734f00d8b16
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-08-26 07:59:04 +0100

    Fixing flatten.

--- teams/nomis/locals.tf
+++ teams/nomis/locals.tf
@@ -27,9 +27,9 @@ locals {
     source-code   = "https://github.com/ministryofjustice/modernisation-platform-ami-builds/tree/main/teams/nomis"
   }
 
-  ami_share_accounts = flatten([
+  ami_share_accounts = flatten([[
     local.environment_management.account_ids["core-shared-services-production"],
     local.environment_management.account_ids["nomis-test"]],
-    var.BRANCH_NAME == "main" ? [local.environment_management.account_ids["nomis-production"]] : [])
+    var.BRANCH_NAME == "main" ? [local.environment_management.account_ids["nomis-production"]] : []])
 
 }
