cb631c51d7f899bbc102c571ab11812976385ac3
SzymonAptacy <szymon@automationlogic.com>
2022-09-06 09:39:30 +0100

    Adding vars missing when renaming the file

--- teams/nomis/rhel_7_9_baseimage_vars.tf
+++ teams/nomis/rhel_7_9_baseimage_vars.tf
@@ -1,5 +1,7 @@
 locals {
   rhel_7_9_baseimage = {
+    gh_actor              = var.GH_ACTOR_NAME
+    branch                = var.BRANCH_NAME
     configuration_version = "1.1.6"
     description           = "nomis RHEL7.9 base image"
 
