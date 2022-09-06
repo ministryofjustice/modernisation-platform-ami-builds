756e3f66f3476722282975f3d3248c56a1646c45
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-09-06 07:23:17 +0100

    Jumpserver pipelines in a separate folder/state

---
+++ teams/nomis/pipelines/jumpserver/data.tf
@@ -0,0 +1,16 @@
+# Retrieve KMS key for AMI/snapshot encryption
+data "aws_kms_key" "hmpps_ebs_encryption_cmk" {
+  key_id = "arn:aws:kms:eu-west-2:${data.aws_caller_identity.current.account_id}:alias/ebs-hmpps"
+}
+
+data "aws_kms_key" "ebs_encryption_cmk" {
+  key_id = "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["nomis-test"]}:alias/nomis-image-builder"
+}
+
+data "aws_launch_template" "weblogic-launch-templates" {
+  provider = aws.nomis-test
+  filter {
+    name   = "tag:environment-name"
+    values = ["nomis-test"]
+  }
+}
