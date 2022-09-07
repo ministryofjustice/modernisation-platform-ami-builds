fe1d28ad2ec2c0bff0e0a76d7fd7dd284da5b2a3
SzymonAptacy <szymon@automationlogic.com>
2022-09-07 15:01:18 +0100

    Add node_exporter installation to rhel 7.9 component

--- teams/nomis/rhel_7_9_baseimage_vars.tf
+++ teams/nomis/rhel_7_9_baseimage_vars.tf
@@ -2,7 +2,7 @@ locals {
   rhel_7_9_baseimage = {
     gh_actor              = var.GH_ACTOR_NAME
     branch                = var.BRANCH_NAME
-    configuration_version = "1.1.6"
+    configuration_version = "1.1.7"
     description           = "nomis RHEL7.9 base image"
 
     tags = {
