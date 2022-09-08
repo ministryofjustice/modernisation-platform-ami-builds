66499a1f9022a0c726eb237520cb6187dd7880b8
Ewa Stempel <ewa.stempel@digital.justice.gov.uk>
2022-09-08 09:25:14 +0100

    Testing - filtering non empty params

--- teams/nomis/pipelines/jumpserver/jumpserver_pipeline.tf
+++ teams/nomis/pipelines/jumpserver/jumpserver_pipeline.tf
@@ -49,7 +49,7 @@ resource "aws_imagebuilder_image_recipe" "jumpserver" {
     content {
       component_arn = aws_imagebuilder_component.jumpserver_components[component.key].arn
       dynamic "parameter"{
-        for_each = [for param in component.value.parameters : param if param != {}]
+        for_each = [for param in component.value.parameters : param if param != null]
         content {
           parameter {
             name = parameter.key
