d98432eae94bf6ab62469eeda9868afda61bd0d8
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-10-20 15:38:18 +0100

    specify distinct terraform version

--- modernisation-platform/versions.tf
+++ modernisation-platform/versions.tf
@@ -1,10 +1,9 @@
 terraform {
-  experiments = [module_variable_optional_attrs]
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-  required_version = "~> 1.0"
+  required_version = "=1.3.3"
 }
