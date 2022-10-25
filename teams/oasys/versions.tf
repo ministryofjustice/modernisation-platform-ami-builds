d9bd26cb80819357c63c074f9b4b6c5df43ed2ab
wullub <william@automationlogic.com>
2022-10-25 15:23:13 +0100

    better organisation

--- teams/oasys/versions.tf
+++ teams/oasys/versions.tf
@@ -8,3 +8,16 @@ terraform {
   }
   required_version = "=1.0.0"
 }
+
+provider "aws" {
+  alias  = "modernisation-platform"
+  region = "eu-west-2"
+}
+
+# AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
+provider "aws" { # PUT THIS IN PROVIDER FILE AND SYMLINK TO DIRS
+  region = "eu-west-2"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/ModernisationPlatformAccess"
+  }
+}
