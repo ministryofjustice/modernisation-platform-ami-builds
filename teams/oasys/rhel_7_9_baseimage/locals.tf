78771ab83109ea2f44e529940e1eb8aebea954af
W <willg258@hotmail.co.uk>
2022-10-27 13:56:04 +0100

    make ami no launch config

--- teams/oasys/rhel_7_9_baseimage/locals.tf
+++ teams/oasys/rhel_7_9_baseimage/locals.tf
@@ -3,32 +3,32 @@ locals {
   distribution_configuration_by_branch = {
     # push to main branch
     main = {
-      ami_distribution_configuration = {
+      ami_distribution_configuration = { # which accounts to distibute the ami
         target_account_ids_or_names = [
           #"core-shared-services-production",
           "oasys-test"
         ]
       }
 
-      launch_template_configuration = {
-        account_id_or_name = "oasys-test"
-        launch_template_id = data.aws_launch_template.rhel_7_9_baseimage.id
-      }
+      # launch_template_configuration = { # this block creates a launch template
+      #   account_id_or_name = "oasys-test"
+      #   launch_template_id = data.aws_launch_template.rhel_7_9_baseimage.id
+      # }
     }
 
     # push to any other branch / local run
     default = {
-      ami_distribution_configuration = {
+      ami_distribution_configuration = { # which accounts to distibute the ami
         target_account_ids_or_names = [
           #"core-shared-services-production",
           "oasys-test"
         ]
       }
 
-      launch_template_configuration = {
-        account_id_or_name = "oasys-test"
-        launch_template_id = data.aws_launch_template.rhel_7_9_baseimage.id
-      }
+      # launch_template_configuration = { # this block creates a launch template
+      #   account_id_or_name = "oasys-test"
+      #   launch_template_id = data.aws_launch_template.rhel_7_9_baseimage.id
+      # }
     }
   }
 }
