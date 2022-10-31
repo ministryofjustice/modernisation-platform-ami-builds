7a9f5e16574a72ea0010a434e3856b9d9eed93c9
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-10-31 16:00:12 +0000

    DSOS-1524: dummy change to test pipeline

--- teams/nomis/rhel_6_10_baseimage/backend.tf
+++ teams/nomis/rhel_6_10_baseimage/backend.tf
@@ -1,4 +1,5 @@
 # Backend
+
 terraform {
   # `backend` blocks do not support variables, so the following are hard-coded here:
   # - S3 bucket name, which is created in modernisation-platform-account/s3.tf
