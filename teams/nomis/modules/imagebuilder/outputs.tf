7725af56438fefc5b8aa6d02462d86012f6a84cd
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-10-31 14:03:41 +0000

    DSOS-1500: redhat arn test

--- teams/nomis/modules/imagebuilder/outputs.tf
+++ teams/nomis/modules/imagebuilder/outputs.tf
@@ -1,4 +1,6 @@
 output "parent_ami" {
-  value       = try(data.aws_ami.parent[0], {})
+  value = try(data.aws_ami.parent[0], {
+    arn = local.ami_parent_arn
+  })
   description = "parent AMI details, useful if looked up by a filter"
 }
