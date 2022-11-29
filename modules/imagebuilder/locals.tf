a87b0f29b90641e23e403afe5221bd2716b0231a
wullub <william@automationlogic.com>
2022-11-29 12:06:11 +0000

    ..

--- modules/imagebuilder/locals.tf
+++ modules/imagebuilder/locals.tf
@@ -15,7 +15,7 @@ locals {
 
   components_custom_data = {
     for component in var.components_custom :
-    component_filename => length(regexall(".*tftpl", component.path)) > 0 ?
+    component_filename => length(regexall(".*tftpl", component["path"])) > 0 ?
     templatefile(component.path, var.component_template_args) :
     file(component.path)
   }
