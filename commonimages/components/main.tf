5f0ac28e2aa4f17c5f4f3dc66b61e4c3abba7cb7
wullub <william@automationlogic.com>
2022-11-29 16:06:07 +0000

    ..

--- commonimages/components/main.tf
+++ commonimages/components/main.tf
@@ -6,7 +6,7 @@ resource "aws_imagebuilder_component" "this" {
   platform    = each.value.yaml.metadata.Platform
   version     = each.value.yaml.metadata.Version
   data        = each.value.raw
-  kms_key_id  = var.kms_key_id
+  kms_key_id  = data.aws_kms_key.hmpps_ebs_encryption_cmk.arn
   tags        = local.tags
 
   lifecycle {
