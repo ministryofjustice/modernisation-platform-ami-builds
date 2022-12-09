d8704d4397dc59f6d6a3349e8f8b6dd0bac24f90
W <willg258@hotmail.co.uk>
2022-12-09 16:52:58 +0000

    appserver use shared module
    appserver use shared module

---
+++ teams/nomis/rhel_6_10_weblogic_appserver_10_3/locals.tf
@@ -0,0 +1,35 @@
+locals {
+  environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
+
+  team_name        = "nomis"
+  application_name = "core-shared-services"
+  environment      = trimprefix(terraform.workspace, "core-shared-services-")
+  provider_name    = "core-vpc-development"
+
+  # these are all based on https://technical-guidance.service.justice.gov.uk/documentation/standards/documenting-infrastructure-owners.html#tags-you-should-use
+  shared_tags = {
+    business-unit = "HMPPS"
+    application   = upper(local.team_name)
+    branch        = var.BRANCH_NAME == "" ? "n/a" : var.BRANCH_NAME
+    github-actor  = var.GH_ACTOR_NAME == "" ? "n/a" : var.GH_ACTOR_NAME
+    is-production = var.BRANCH_NAME == "main" ? "true" : "false"
+    owner         = "DSO: digital-studio-operations-team@digital.justice.gov.uk"
+    source-code   = "https://github.com/ministryofjustice/modernisation-platform-ami-builds/tree/main/teams/nomis"
+  }
+
+  components_common = [
+    {
+      name    = "ansible"
+      version = "0.0.1"
+      parameters = [{
+        name  = "Ami"
+        value = join("_", [var.ami_name_prefix, var.ami_base_name])
+        }, {
+        name  = "Branch"
+        value = var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
+      }]
+    }
+  ]
+
+  component_template_args = {}
+}
