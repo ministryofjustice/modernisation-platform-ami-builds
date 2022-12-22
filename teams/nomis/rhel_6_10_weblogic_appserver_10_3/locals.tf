32c23e6a09f61919faec648b6bf85d2fadcbd557
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-12-22 11:45:36 +0000

    DSOS-1611: fix

--- teams/nomis/rhel_6_10_weblogic_appserver_10_3/locals.tf
+++ teams/nomis/rhel_6_10_weblogic_appserver_10_3/locals.tf
@@ -10,17 +10,6 @@ locals {
         }, {
         name  = "Branch"
         value = var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
-      }]
-    },
-    {
-      name    = "ansible"
-      version = "0.0.3"
-      parameters = [{
-        name  = "Ami"
-        value = join("_", [var.ami_name_prefix, var.ami_base_name])
-        }, {
-        name  = "Branch"
-        value = var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
         }, {
         name  = "AnsibleRepo"
         value = "modernisation-platform-configuration-management"
