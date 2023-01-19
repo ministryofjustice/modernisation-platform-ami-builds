1c76d6d75171929faea440f4fb381b5d45a19281
James Borwick <james.borwick@icloud.com>
2023-01-19 14:45:36 +0000

    Removing parameters for components_common

--- teams/nomis/windows_server_2019_jumpserver/locals.tf
+++ teams/nomis/windows_server_2019_jumpserver/locals.tf
@@ -3,24 +3,12 @@ locals {
     {
       name    = "prometheus_windows_exporter"
       version = "1.1.7"
-      parameters = [{
-        name  = "Ami"
-        value = join("_", [var.ami_name_prefix, var.ami_base_name])
-        }, {
-        name  = "Branch"
-        value = var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
-      }]
+      parameters = []
     },
     {
       name    = "powershell_core"
       version = "0.1.9"
-      parameters = [{
-        name  = "Ami"
-        value = join("_", [var.ami_name_prefix, var.ami_base_name])
-        }, {
-        name  = "Branch"
-        value = var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
-      }]
+      parameters = []
     },
   ]
 
