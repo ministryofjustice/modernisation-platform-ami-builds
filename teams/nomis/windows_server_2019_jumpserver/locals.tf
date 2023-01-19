375b1a19cec11919214d2fbb5be71e864eee10e3
James Borwick <james.borwick@icloud.com>
2023-01-19 14:24:18 +0000

    Incremented component version

--- teams/nomis/windows_server_2019_jumpserver/locals.tf
+++ teams/nomis/windows_server_2019_jumpserver/locals.tf
@@ -1,5 +1,28 @@
 locals {
-  components_common = []
+  components_common = [
+    {
+      name    = "prometheus_windows_exporter"
+      version = "1.1.7"
+      parameters = [{
+        name  = "Ami"
+        value = join("_", [var.ami_name_prefix, var.ami_base_name])
+        }, {
+        name  = "Branch"
+        value = var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
+      }]
+    },
+    {
+      name    = "powershell_core"
+      version = "0.1.9"
+      parameters = [{
+        name  = "Ami"
+        value = join("_", [var.ami_name_prefix, var.ami_base_name])
+        }, {
+        name  = "Branch"
+        value = var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
+      }]
+    },
+  ]
 
   component_template_args = {}
 }
--- teams/nomis/windows_server_2019_jumpserver/terraform.tfvars
+++ teams/nomis/windows_server_2019_jumpserver/terraform.tfvars
@@ -33,12 +33,6 @@ components_aws = [
 
 components_custom = [
   {
-    path       = "../../commonimages/components/templates/powershell_core.yml"
-    parameters = []
-    }, {
-    path       = "../../commonimages/components/templates/prometheus_windows_exporter.yml"
-    parameters = []
-    }, {
     path       = "./components/windows_server_2019_jumpserver/jumpserver_2019.yml"
     parameters = []
   }
