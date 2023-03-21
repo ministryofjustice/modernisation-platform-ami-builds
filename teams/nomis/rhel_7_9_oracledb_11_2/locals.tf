cbe5c260b8317829501cbb5f46d95fade47633e8
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-03-21 16:44:35 +0000

    test oracle db image

--- teams/nomis/rhel_7_9_oracledb_11_2/locals.tf
+++ teams/nomis/rhel_7_9_oracledb_11_2/locals.tf
@@ -3,13 +3,14 @@ locals {
   components_common = [
     {
       name    = "ansible"
-      version = "0.0.2"
+      version = "0.0.5"
       parameters = [{
         name  = "Ami"
         value = join("_", [var.ami_name_prefix, var.ami_base_name])
         }, {
-        name  = "Branch"
-        value = var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
+        name = "Branch"
+        # value = "main" # replace main with corresponding ansible branch if you are testing
+        value = "nomis/DSOS-1817/oracle-11g-ami-update"
       }]
     }
   ]
