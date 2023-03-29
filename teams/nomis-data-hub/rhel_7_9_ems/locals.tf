51dfb72326867faa9465fe415f5a68af26924f47
Antony Gowland <36728441+antonygowland@users.noreply.github.com>
2023-03-29 14:28:57 +0100

    DSOS-1506: use test branch

--- teams/nomis-data-hub/rhel_7_9_ems/locals.tf
+++ teams/nomis-data-hub/rhel_7_9_ems/locals.tf
@@ -9,7 +9,7 @@ locals {
         value = join("_", [var.ami_name_prefix, var.ami_base_name])
         }, {
         name  = "Branch"
-        value = "main" # replace main with corresponding ansible branch if you are testing
+        value = "feature/DSOS-1506/test_ami" # replace main with corresponding ansible branch if you are testing
       }]
     }
   ]
