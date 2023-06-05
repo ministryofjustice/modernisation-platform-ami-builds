aaa7a3110447873cc28f2e217378db4c8f5f9604
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-06-05 17:11:13 +0100

    Improvements and testing of hmpps delius core

--- teams/delius-core/oracle_db/locals.tf
+++ teams/delius-core/oracle_db/locals.tf
@@ -9,7 +9,10 @@ locals {
         value = join("_", [var.ami_name_prefix, var.ami_base_name])
         }, {
         name  = "Branch"
-        value = "nit676-add-delius-core-oracle-db-config" # Custom branch added for testing, was: var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
+        value = "nit677-hmpps-delius-core-db-improvements" # Custom branch added for testing, was: var.BRANCH_NAME == "" ? "main" : var.BRANCH_NAME
+        }, {
+        name  = "AnsibleTags"
+        value = "amibuild,oracle_19c_download"
       }]
     }
   ]
