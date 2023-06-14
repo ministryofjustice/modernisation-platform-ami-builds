8bb0fdc9fc64e2d2c7826cb5060141dda16a27ad
David Sibley <david.sibley@digital.justice.gov.uk>
2023-06-14 09:36:00 +0100

    added further versions. tested example code showing no changes

--- teams/sprinkler/versions.tf
+++ teams/sprinkler/versions.tf
@@ -1,9 +1,8 @@
 terraform {
-  experiments = [module_variable_optional_attrs]
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
   required_version = "~> 1.0"
