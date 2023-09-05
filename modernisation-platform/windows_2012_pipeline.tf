39648f1e26ab268d9953100953f91644c46edf74
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-09-05 14:07:36 +0100

    specify pipeline

--- modernisation-platform/windows_2012_pipeline.tf
+++ modernisation-platform/windows_2012_pipeline.tf
@@ -2,10 +2,10 @@ resource "aws_imagebuilder_image_pipeline" "windowsserver2012" {
   image_recipe_arn                 = aws_imagebuilder_image_recipe.windowsserver2012.arn
   infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.windowsserver2012.arn
   distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.windowsserver2012.arn
-  name                             = local.windows_pipeline.pipeline.name
+  name                             = local.windows_2012_pipeline.pipeline.name
 
   schedule {
-    schedule_expression                = local.windows_pipeline.pipeline.schedule
+    schedule_expression                = local.windows_2012_pipeline.pipeline.schedule
     pipeline_execution_start_condition = "EXPRESSION_MATCH_AND_DEPENDENCY_UPDATES_AVAILABLE"
   }
 
@@ -23,26 +23,26 @@ resource "aws_imagebuilder_image" "windowsserver2012" {
 
 resource "aws_imagebuilder_image_recipe" "windowsserver2012" {
   block_device_mapping {
-    device_name = local.windows_pipeline.recipe.device_name
+    device_name = local.windows_2012_pipeline.recipe.device_name
 
     ebs {
-      delete_on_termination = local.windows_pipeline.recipe.ebs.delete_on_termination
-      volume_size           = local.windows_pipeline.recipe.ebs.volume_size
-      volume_type           = local.windows_pipeline.recipe.ebs.volume_type
-      encrypted             = local.windows_pipeline.recipe.ebs.encrypted
+      delete_on_termination = local.windows_2012_pipeline.recipe.ebs.delete_on_termination
+      volume_size           = local.windows_2012_pipeline.recipe.ebs.volume_size
+      volume_type           = local.windows_2012_pipeline.recipe.ebs.volume_type
+      encrypted             = local.windows_2012_pipeline.recipe.ebs.encrypted
       kms_key_id            = data.aws_kms_key.ebs_encryption_cmk.arn
     }
   }
 
   dynamic "component" {
-    for_each = toset(local.windows_pipeline.components)
+    for_each = toset(local.windows_2012_pipeline.components)
     content {
       component_arn = aws_imagebuilder_component.windowsserver2012_components[component.key].arn
     }
   }
 
   dynamic "component" {
-    for_each = toset(local.windows_pipeline.aws_components)
+    for_each = toset(local.windows_2012_pipeline.aws_components)
     content {
       component_arn = "arn:aws:imagebuilder:eu-west-2:aws:component/${component.key}/x.x.x"
     }
@@ -52,20 +52,20 @@ resource "aws_imagebuilder_image_recipe" "windowsserver2012" {
     create_before_destroy = true
   }
 
-  name         = local.windows_pipeline.recipe.name
-  parent_image = local.windows_pipeline.recipe.parent_image
-  version      = local.windows_pipeline.recipe.version
+  name         = local.windows_2012_pipeline.recipe.name
+  parent_image = local.windows_2012_pipeline.recipe.parent_image
+  version      = local.windows_2012_pipeline.recipe.version
 }
 
 
 resource "aws_imagebuilder_infrastructure_configuration" "windowsserver2012" {
-  description                   = local.windows_pipeline.infra_config.description
+  description                   = local.windows_2012_pipeline.infra_config.description
   instance_profile_name         = aws_iam_instance_profile.image_builder_profile.name
-  instance_types                = local.windows_pipeline.infra_config.instance_types
-  name                          = local.windows_pipeline.infra_config.name
-  security_group_ids            = local.windows_pipeline.infra_config.security_group_ids
-  subnet_id                     = local.windows_pipeline.infra_config.subnet_id
-  terminate_instance_on_failure = local.windows_pipeline.infra_config.terminate_on_fail
+  instance_types                = local.windows_2012_pipeline.infra_config.instance_types
+  name                          = local.windows_2012_pipeline.infra_config.name
+  security_group_ids            = local.windows_2012_pipeline.infra_config.security_group_ids
+  subnet_id                     = local.windows_2012_pipeline.infra_config.subnet_id
+  terminate_instance_on_failure = local.windows_2012_pipeline.infra_config.terminate_on_fail
 
   logging {
     s3_logs {
@@ -77,7 +77,7 @@ resource "aws_imagebuilder_infrastructure_configuration" "windowsserver2012" {
 
 // create each component in team directory
 resource "aws_imagebuilder_component" "windowsserver2012_components" {
-  for_each = { for file in local.windows_pipeline.components : file => yamldecode(file("components/windows/${file}")) }
+  for_each = { for file in local.windows_2012_pipeline.components : file => yamldecode(file("components/windows/${file}")) }
 
   data       = file("components/windows/${each.key}")
   name       = join("_", ["mp", trimsuffix(each.key, ".yml")])
@@ -93,14 +93,14 @@ resource "aws_imagebuilder_component" "windowsserver2012_components" {
 
 
 resource "aws_imagebuilder_distribution_configuration" "windowsserver2012" {
-  name = local.windows_pipeline.distribution.name
+  name = local.windows_2012_pipeline.distribution.name
 
   distribution {
-    region = local.windows_pipeline.distribution.region
+    region = local.windows_2012_pipeline.distribution.region
 
     ami_distribution_configuration {
 
-      name       = local.windows_pipeline.distribution.ami_name
+      name       = local.windows_2012_pipeline.distribution.ami_name
       kms_key_id = data.aws_kms_key.ebs_encryption_cmk.arn
 
       launch_permission {
