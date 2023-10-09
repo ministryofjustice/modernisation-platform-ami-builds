5761a11db045c6f8733d5208b11054ddb0e7b7c1
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-10-09 13:21:50 +0100

    remove outputs used for debug

--- commonimages/base/output.tf
+++
@@ -1,4 +0,0 @@
-output "image_builder" {
-  value       = module.imagebuilder.image_builder_security_group
-  description = "Output the entire object to inspect its attributes"
-}
--- modules/imagebuilder/output.tf
+++
@@ -1,4 +0,0 @@
-output "image_builder_security_group" {
-  value       = [local.core_shared_services.repo_tfstate.image_builder_security_group_id]
-  description = "Output the entire object to inspect its attributes"
-}
