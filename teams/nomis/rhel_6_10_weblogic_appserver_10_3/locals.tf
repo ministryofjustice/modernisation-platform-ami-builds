4e8bcb5497e075dcfc6f514503a7b6f0233ac443
Shajida Akthar <Shajida Akthar>
2023-11-29 15:42:51 +0000

    Updated ansible component version for nomis rhel_6_10

--- teams/nomis/rhel_6_10_weblogic_appserver_10_3/locals.tf
+++ teams/nomis/rhel_6_10_weblogic_appserver_10_3/locals.tf
@@ -3,13 +3,13 @@ locals {
   components_common = [
     {
       name    = "ansible"
-      version = "0.0.5"
+      version = "0.0.9"
       parameters = [{
         name  = "Ami"
         value = join("_", [var.ami_name_prefix, var.ami_base_name])
         }, {
         name  = "Branch"
-        value = "main" # replace main with corresponding ansible branch if you are testing
+        value = "dso-2239/ansible-failure-handling-in-ami-builds" # replace main with corresponding ansible branch if you are testing
       }]
     }
   ]
