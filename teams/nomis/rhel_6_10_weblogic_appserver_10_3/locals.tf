574222f01266c7626c096a587349d08a013eab1f
Shajida Akthar <Shajida Akthar>
2023-12-20 14:38:09 +0000

    use custom component only

--- teams/nomis/rhel_6_10_weblogic_appserver_10_3/locals.tf
+++ teams/nomis/rhel_6_10_weblogic_appserver_10_3/locals.tf
@@ -1,18 +1,6 @@
 locals {
 
-  components_common = [
-    {
-      name    = "ansible"
-      version = "0.0.11"
-      parameters = [{
-        name  = "Ami"
-        value = join("_", [var.ami_name_prefix, var.ami_base_name])
-        }, {
-        name  = "Branch"
-        value = "main" # replace main with corresponding ansible branch if you are testing
-      }]
-    }
-  ]
+  components_common = []
 
   component_template_args = {
     ami     = join("_", [var.ami_name_prefix, var.ami_base_name])
