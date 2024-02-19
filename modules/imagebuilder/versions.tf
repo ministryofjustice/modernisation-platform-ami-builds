9e4a349b199af28070c28e38da42757e4a45f5ee
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-02-19 10:32:00 +0000

    remove provider blocks from module

--- modules/imagebuilder/versions.tf
+++ modules/imagebuilder/versions.tf
@@ -8,15 +8,15 @@ terraform {
   required_version = "=1.5.7"
 }
 
-provider "aws" {
-  alias  = "modernisation-platform"
-  region = "eu-west-2"
-}
+# provider "aws" {
+#   alias  = "modernisation-platform"
+#   region = "eu-west-2"
+# }
 
-# AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
-provider "aws" {
-  region = "eu-west-2"
-  assume_role {
-    role_arn = "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/ModernisationPlatformAccess"
-  }
-}
+# # AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
+# provider "aws" {
+#   region = "eu-west-2"
+#   assume_role {
+#     role_arn = "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/ModernisationPlatformAccess"
+#   }
+# }
