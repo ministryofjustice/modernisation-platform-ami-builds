382b6c84f19be955ad02f0fe7ce882f9ea2093af
Aaron Robinson <aaron.robinson1@justice.gov.uk>
2025-07-21 12:34:57 +0100

    Update to v6 aws provider

--- modules/imagebuilder/versions.tf
+++ modules/imagebuilder/versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
-  required_version = "=1.5.7"
+  required_version = "~> 1.0"
 }
 
 provider "aws" {
